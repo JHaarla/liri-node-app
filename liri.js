da8ec1629da25c02cd02bc554cf9f86150049af1
JHaarla <haarladesign@gmail.com>
2019-09-24 20:33:12 -0400

    commit beforte starting code overhaul to add part 4

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ let keys = require("./keys.js");
 let axios = require("axios");
 let moment = require("moment");
 let Spotify = require('node-spotify-api');
+let fs = require("fs");
 
 let inputString = process.argv;
 
@@ -25,36 +26,36 @@ switch (liriType) {
 
         if (movieName === "") {
             axios
-            .get("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=1d4cdfb1")
-            .then(function(response) {
-                console.log("You didn't enter a movie but if you haven't checked out Mr. Nobody, you should...");
-                console.log("=================================");
-                console.log("Title: " + response.data.Title);
-                console.log("Release Year: " + response.data.Year);
-                console.log("IMDB Rating: " + response.data.imdbRating);
-                console.log("Rotten Tomatoes Rating:: " + response.data.Ratings[1].Value);
-                console.log("Produced in: " + response.data.Country);
-                console.log("Language: " + response.data.Language);
-                console.log("Plot: " + response.data.Plot);
-                console.log("Actors: " + response.data.Actors);
-                console.log("=================================\n");
-            })
+                .get("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=1d4cdfb1")
+                .then(function (response) {
+                    console.log("You didn't enter a movie but if you haven't checked out Mr. Nobody, you should...");
+                    console.log("=================================");
+                    console.log("Title: " + response.data.Title);
+                    console.log("Release Year: " + response.data.Year);
+                    console.log("IMDB Rating: " + response.data.imdbRating);
+                    console.log("Rotten Tomatoes Rating:: " + response.data.Ratings[1].Value);
+                    console.log("Produced in: " + response.data.Country);
+                    console.log("Language: " + response.data.Language);
+                    console.log("Plot: " + response.data.Plot);
+                    console.log("Actors: " + response.data.Actors);
+                    console.log("=================================\n");
+                })
         } else {
 
-        axios
-            .get(queryUrl)
-            .then(function(response) {
-                console.log("\n=================================");
-                console.log("Title: " + response.data.Title);
-                console.log("Release Year: " + response.data.Year);
-                console.log("IMDB Rating: " + response.data.imdbRating);
-                console.log("Rotten Tomatoes Rating:: " + response.data.Ratings[1].Value);
-                console.log("Produced in: " + response.data.Country);
-                console.log("Language: " + response.data.Language);
-                console.log("Plot: " + response.data.Plot);
-                console.log("Actors: " + response.data.Actors);
-                console.log("=================================\n");
-            })
+            axios
+                .get(queryUrl)
+                .then(function (response) {
+                    console.log("\n=================================");
+                    console.log("Title: " + response.data.Title);
+                    console.log("Release Year: " + response.data.Year);
+                    console.log("IMDB Rating: " + response.data.imdbRating);
+                    console.log("Rotten Tomatoes Rating:: " + response.data.Ratings[1].Value);
+                    console.log("Produced in: " + response.data.Country);
+                    console.log("Language: " + response.data.Language);
+                    console.log("Plot: " + response.data.Plot);
+                    console.log("Actors: " + response.data.Actors);
+                    console.log("=================================\n");
+                })
         }
         break;
 
@@ -78,34 +79,34 @@ switch (liriType) {
         }
 
         const titleCase = title => title
-        .split(/ /g).map(word => 
-        `${word.substring(0,1).toUpperCase()}${word.substring(1)}`)
-        .join(" ");
+            .split(/ /g).map(word =>
+                `${word.substring(0, 1).toUpperCase()}${word.substring(1)}`)
+            .join(" ");
         let artistCaps = titleCase(artistFormatted);
 
         let artistQueryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=1e4b7dcd038e8151a1fd002ec56540b2";
         console.log(artistQueryUrl);
 
         axios
-        .get(artistQueryUrl)
-        .then(function(response) {
+            .get(artistQueryUrl)
+            .then(function (response) {
 
-            console.log("Here is " + artistCaps + "'s next concert:");
-            console.log("=================================");
-            console.log("Lineup: " + response.data[0].lineup);
-            console.log("Venue: " + response.data[0].venue.name);
-            console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region + ", " + response.data[0].venue.country);
-            console.log("Date: " + moment(response.data[0].datetime).format(' MM DD YYYY'));
-            console.log("=================================\n");
-        })
-        .catch(function(error) {
-            if (error.response) {
-              }
-              console.log("=================================");
-              console.log("I didn't find any concerts. Double check your spelling, maybe?");
-              console.log("=================================");
+                console.log("Here is " + artistCaps + "'s next concert:");
+                console.log("=================================");
+                console.log("Lineup: " + response.data[0].lineup);
+                console.log("Venue: " + response.data[0].venue.name);
+                console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region + ", " + response.data[0].venue.country);
+                console.log("Date: " + moment(response.data[0].datetime).format(' MM DD YYYY'));
+                console.log("=================================\n");
+            })
+            .catch(function (error) {
+                if (error.response) {
+                }
+                console.log("=================================");
+                console.log("I didn't find any concerts. Double check your spelling, maybe?");
+                console.log("=================================");
 
-        })
+            })
         break;
 
     case "spotify-this-song":
@@ -119,51 +120,49 @@ switch (liriType) {
             }
         }
 
-        // console.log(trackName);
+        if (trackName !== "") {
 
-        let spotify = new Spotify(keys.spotify);
+            // console.log(trackName);
 
-        spotify.search({ type: 'track', query: trackName, limit: 3 }, function(err, data) {
-            if (err) {
-              return console.log('Error occurred: ' + err);
-            }
-           
-        //   console.log(data); 
-        //   console.log(data.tracks); 
-        //   console.log(data.tracks.items[0]); 
-        //   console.log(data.tracks.items[0].album.name); 
+            let spotify = new Spotify(keys.spotify);
 
+            spotify.search({ type: 'track', query: trackName, limit: 3 }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
 
-          console.log("Artist: " + data.tracks.items[0].album.artists[0].name); 
-          console.log("Song name: " + data.tracks.items[0].name); 
-          console.log("Album: " + data.tracks.items[0].album.name); 
-          console.log("Song preview: " + data.tracks.items[0].preview_url); 
-
-          });
+                //   console.log(data); 
+                //   console.log(data.tracks); 
+                //   console.log(data.tracks.items[0]); 
+                //   console.log(data.tracks.items[0].album.name); 
 
 
+                console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+                console.log("Song name: " + data.tracks.items[0].name);
+                console.log("Album: " + data.tracks.items[0].album.name);
+                console.log("Song preview: " + data.tracks.items[0].preview_url);
 
+            });
+        } else {
+            let spotify = new Spotify(keys.spotify);
+
+            spotify.search({ type: 'track', query: "Ace of base", limit: 3 }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                console.log("\n=================================");
+                console.log("You didn't put in a song to search for... Here's an old favorite of ours:")
+                console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+                console.log("Song name: " + data.tracks.items[0].name);
+                console.log("Album: " + data.tracks.items[0].album.name);
+                console.log("Song preview: " + data.tracks.items[0].preview_url);
+                console.log("=================================\n");
+
+            });
+        }
+
+        break;
+
+        // case "spotify-this-song":
 
 }
-
-
-
-
-
-
-
-
-
-
-
-// var Spotify = require('node-spotify-api');
- 
-// var spotify = new Spotify(keys.secret);
- 
-// spotify.search({ type: 'track', query: 'wow', limit: 1 }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
- 
-// console.log(data); 
-// });
